bcbdfbaa4a35ebca10e20a6b668530437567c7d6
arturomoreno <arturomoreno151@gmail.com>
2016-09-06 17:36:37 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+#include "sfwdraw.h"
+
+using namespace sfw;
+
+
+void main()
+{
+	initContext(800,600,"pong");
+
+
+	float acc = 0;
+	char c = '\0';
+	
+	setBackgroundColor(BLACK);
+	
+	while (stepContext())
+	{	
+		if (getKey('W'));
+		{
+			acc += getDeltaTime() * 500;
+		}
+		if (getKey('S'))
+		{
+
+			acc -= getDeltaTime() * 500;
+
+		}
+
+		drawLine(10, 300 + acc, 10, 250 + acc, WHITE);
+
+
+	}
+
+	termContext();
+}
